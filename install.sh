d51b0bfd02d7931669237ca9adca5c277686ab68
8bitgrumpy <71152299+8bitgrumpy@users.noreply.github.com>
2020-09-12 16:16:15 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-pacman -S nvidia i3-gaps i3lock htop compton git pulseaudio networkmanager networkmanager-openconnect network-manager-applet alsa pavucontrol zsh grml-zsh-config vlc gimp virtualbox neofetch firefox alsa-lib broadcom-wl rxvt-unicode xorg
-
